0966474f78caa9a0715138d696ff22d81be28350
ashishvartak <vartak.a@gmail.com>
2022-04-09 14:23:15 +0530

    ContainerChild

--- expt2/lib/main.dart
+++ expt2/lib/main.dart
@@ -1,64 +1,21 @@
 import 'package:flutter/material.dart';
 
-//Example for Column Widget
-void main() {
-  runApp(MyApp());
-}
+//This Example is for demonstrating child property of Container
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FAMT',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Container example"),
+        ),
+        body: Container(
+          child: Text("Hello! i am inside a container!",
+              style: TextStyle(fontSize: 20)),
+        ),
       ),
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("IT Department"),
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.green),
-              child: Text(
-                "TEIT",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.green),
-              child: Text(
-                "TEIT",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.green),
-              child: Text(
-                "TEIT",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            )
-          ]),
     );
   }
 }
