e0c363049ff9cfdab5dec1cef31dd60c0e05829a
ashishvartak <vartak.a@gmail.com>
2022-04-09 14:29:08 +0530

    ContainerHeightWidth

--- expt2/lib/main.dart
+++ expt2/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-//This Example is for demonstrating color property of Container
+//This Example is for demonstrating Height and Width property of Container
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
           title: Text("Container example"),
         ),
         body: Container(
+          height: 200,
+          width: double.infinity,
           color: Colors.purple,
           child: Text("Hello! i am inside a container!",
               style: TextStyle(fontSize: 20)),
