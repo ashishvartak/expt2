30feae0de966d1c03bb6e232c7d0bc3f0cd1fcab
ashishvartak <vartak.a@gmail.com>
2022-04-09 12:44:06 +0530

    AppBar

--- expt2/lib/main.dart
+++ expt2/lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-//This example exlains view of scattered App without using SafeArea
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Text(
-          'This is an example explaining use of SafeArea',
-          style: TextStyle(color: Colors.green, fontSize: 20),
-        ),
-      ),
-    );
-  }
+//Use of AppBar Example 1
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('FAMT'),
+      ), //AppBar
+      body: const Center(
+        child: Text(
+          'IT Department',
+          style: TextStyle(fontSize: 24),
+        ), //Text
+      ), // center
+    ), //Scaffold
+    debugShowCheckedModeBanner: false, //Removing Debug Banner
+  )); //MaterialApp
 }
