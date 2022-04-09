dd503b70917dea40dc2b5055262975f99d366c91
ashishvartak <vartak.a@gmail.com>
2022-04-09 13:27:05 +0530

    AppBarExpt2

--- expt2/lib/main.dart
+++ expt2/lib/main.dart
@@ -1,18 +1,24 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-//Use of AppBar Example 1
+//Use of AppBar Example 2
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('FAMT'),
+        title: Text("FAMT"),
+        titleSpacing: 00.0,
+        centerTitle: true,
+        toolbarHeight: 60.2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(360)),
+        elevation: 0.00,
+        backgroundColor: Colors.greenAccent[400],
       ), //AppBar
       body: const Center(
         child: Text(
           'IT Department',
           style: TextStyle(fontSize: 24),
         ), //Text
-      ), // center
+      ), //Center
     ), //Scaffold
     debugShowCheckedModeBanner: false, //Removing Debug Banner
   )); //MaterialApp
