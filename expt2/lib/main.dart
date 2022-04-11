2445c7c7ed87f4f6896c5b57b5ecaf006a880452
ashishvartak <vartak.a@gmail.com>
2022-04-11 14:37:10 +0530

    commit

--- expt2/lib/main.dart
+++ expt2/lib/main.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
 
-//This example explains Floating Action Button Widget
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Floating Action Button"),
-          backgroundColor: Colors.blue,
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.share),
-          backgroundColor: Colors.blueAccent,
-          foregroundColor: Colors.white,
-          onPressed: () => {},
-        ),
-        // floatingActionButton: FloatingActionButton.extended(
-        //   onPressed: () {},
-        //   icon: Icon(Icons.account_circle),
-        //   label: Text("Profile"),
-        // ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: Scaffold(
+        body: Center(
+          child: MyHomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? dropdownvalue = 'Flutter';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownvalue,
+      icon: Icon(Icons.arrow_drop_down),
+      iconSize: 25,
+      elevation: 16,
+      style: TextStyle(color: Colors.blue),
+      underline: Container(
+        height: 3,
+        color: Colors.blueAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownvalue = newValue;
+        });
+      },
+      items: <String>["Flutter", "Java", "Android", "Kotlin", "React Native"]
+          .map<DropdownMenuItem<String>>((String val) {
+        // ignore: missing_required_param
+        return DropdownMenuItem<String>(
+          value: val,
+          child: Text(val),
+        );
+      }).toList(),
     );
   }
 }
